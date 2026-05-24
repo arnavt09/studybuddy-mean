cf13d804bf3acfe9e159e848a98580f7dbaaeeb0
arnav <arnavtripathy09@gmail.com>
2026-05-24 17:59:31 -0400

    Completed StudyBuddy backend API and Angular frontend setup

--- frontend/studybuddy-ui/src/app/app.routes.ts
+++ frontend/studybuddy-ui/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TaskList } from './components/task-list/task-list';
+import { AddTask } from './components/add-task/add-task';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'tasks', pathMatch: 'full' },
+  { path: 'tasks', component: TaskList },
+  { path: 'add-task', component: AddTask }
+];
